b14faf39a4bf2373af23ab4823853afd3c8e53df
jhoandrojas <rojasalmariojhoandvid@gmail.com>
2021-09-26 21:26:22 -0500

    prueba

---
+++ src/main/java/com/prueba/springboot1/app/Prueba1Application.java
@@ -0,0 +1,13 @@
+package com.prueba.springboot1.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prueba1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prueba1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba/springboot1/app/Prueba1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.springboot1.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prueba1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
